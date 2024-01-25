2587d9052027a7e6266c0bd3940bba1d97150c0d
Ridge <ridgemuturi@gmail.com>
2024-01-25 15:59:30 +0300

    Initial Commit

---
+++ home/views.py
@@ -0,0 +1,8 @@
+import datetime
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'home/welcome.html',{'today': datetime.today()})
