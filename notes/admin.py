90ab1e12fa53adc926ed62e0b03e5aacce23615e
Ridge <ridgemuturi@gmail.com>
2024-02-09 16:40:03 +0300

    Created the first model

---
+++ notes/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from . import models
+
+
+# Register your models here.
+class NotesAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(models.Notes, NotesAdmin)
