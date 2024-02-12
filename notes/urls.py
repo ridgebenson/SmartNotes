2e9c4f3eabb7f831dbb9598fe984d92b496c7b1f
Ridge <ridgemuturi@gmail.com>
2024-02-12 15:20:47 +0300

    Displayed the content of a note

--- notes/urls.py
+++ notes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes',views.list),
+    path('notes', views.list),
+    path('notes/<int:note_id>', views.detail),
     ]
--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Notes
+from django.http import Http404
 
 
 # Create your views here.
@@ -7,3 +8,11 @@ from .models import Notes
 def list(request):
     all_notes = Notes.objects.all()
     return render(request, 'notes_list.html', {'notes': all_notes})
+
+
+def detail(request, note_id):
+    try:
+        note = Notes.objects.get(pk=note_id)
+    except Notes.DoesNotExist:
+        raise Http404('Note does not exist')
+    return render(request, 'notes_detail.html', {'note': note})
