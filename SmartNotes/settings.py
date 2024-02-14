c62d5b234788badd59373ff75f67c95b51acd9c1
Ridge <ridgemuturi@gmail.com>
2024-02-14 16:01:45 +0300

    Bug fixes

--- SmartNotes/settings.py
+++ SmartNotes/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-&&u)l@o!g)v!!w)!jk307w_3v)g_-jj26f88mfmmfdexq_t-3z
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -57,7 +55,9 @@ ROOT_URLCONF = 'SmartNotes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'static/templates/']
+        'DIRS': [BASE_DIR / 'templates',
+                 BASE_DIR / 'static/templates',
+                 ]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -73,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'SmartNotes.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -83,7 +82,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -103,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -114,7 +111,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
