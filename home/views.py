bd2c9aa99b2a6f1b42e3bf3b1e98af3d43638573
Ridge <ridgemuturi@gmail.com>
2024-02-19 15:29:43 +0300

    Added signup page

--- home/views.py
+++ home/views.py
@@ -1,16 +1,34 @@
 from datetime import datetime
+
+from django.contrib.auth.forms import UserCreationForm
 # from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import CreateView
+from django.shortcuts import redirect
+
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'home/register.html'
+    success_url = '/smart/notes'
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('notes.list')
+        return super().get(request, *args, **kwargs)
+
 
 class LoginInterfaceView(LoginView):
     template_name = 'home/login.html'
 
+
 class LogoutInterfaceView(LogoutView):
     template_name = 'home/logout.html'
+
 
 class HomeView(TemplateView):
     template_name = 'home/welcome.html'
