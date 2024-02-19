762a84c06d401102a5cc5cbbcf4fbe91b724e1cf
Ridge <ridgemuturi@gmail.com>
2024-02-19 14:31:01 +0300

    Set a default user in notes

--- notes/models.py
+++ notes/models.py
@@ -7,7 +7,7 @@ class Notes(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='notes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='notes',default=1)
     # updated_at = models.DateTimeField(auto_now=True)
 
     # def __str__(self):
